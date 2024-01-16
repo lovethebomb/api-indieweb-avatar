86665dd57a5f5f0b4efbff704c22b5e7369c544b
Lucas Heymès <lucas@lovethebomb.eu>
2024-01-16 20:20:45 +0100

    Replace svg by base64 png

--- functions/avatar/index.js
+++ functions/avatar/index.js
@@ -37,16 +37,17 @@ async function handler(event, context) {
   } catch (error) {
     console.log("Error", error);
 
+    const img = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAGpJREFUWEftlUEOABAMBPX/jyaujWg3QSMZV2zXqGWteFhx/YYBCEAgQ6C7pxrtkdZHYrO2JKiux0CGwNW0xgAEIPAFAR/Fai5sD5khgIHnBNTvN+qJrd6qBzBQTiC606PzmRw4WtCLYWAA4goUIYfY4WEAAAAASUVORK5CYII=", 'base64')
     return {
       // We need to return 200 here or Firefox won’t display the image
       // HOWEVER a 200 means that if it times out on the first attempt it will stay the default image until the next build.
       statusCode: 200,
       headers: {
         "content-type": "image/png",
+        "content-length": img.length,
         "x-error-message": error.message
       },
-      body: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAGpJREFUWEftlUEOABAMBPX/jyaujWg3QSMZV2zXqGWteFhx/YYBCEAgQ6C7pxrtkdZHYrO2JKiux0CGwNW0xgAEIPAFAR/Fai5sD5khgIHnBNTvN+qJrd6qBzBQTiC606PzmRw4WtCLYWAA4goUIYfY4WEAAAAASUVORK5CYII=`,
-      isBase64Encoded: false,
+      body: img,
     };
   }
 }
