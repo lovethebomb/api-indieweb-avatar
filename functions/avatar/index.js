6fd8e7c5633aad8fb4759aa3f7da94a5fcc092fb
Lucas Heymès <lucas@lovethebomb.eu>
2025-05-22 22:29:11 +0200

    add bg to image

--- functions/avatar/index.js
+++ functions/avatar/index.js
@@ -37,7 +37,8 @@ async function handler(event, context) {
   } catch (error) {
     console.log("Error", error);
 
-    const img = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAGpJREFUWEftlUEOABAMBPX/jyaujWg3QSMZV2zXqGWteFhx/YYBCEAgQ6C7pxrtkdZHYrO2JKiux0CGwNW0xgAEIPAFAR/Fai5sD5khgIHnBNTvN+qJrd6qBzBQTiC606PzmRw4WtCLYWAA4goUIYfY4WEAAAAASUVORK5CYII=", 'base64')
+    // const img = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAGpJREFUWEftlUEOABAMBPX/jyaujWg3QSMZV2zXqGWteFhx/YYBCEAgQ6C7pxrtkdZHYrO2JKiux0CGwNW0xgAEIPAFAR/Fai5sD5khgIHnBNTvN+qJrd6qBzBQTiC606PzmRw4WtCLYWAA4goUIYfY4WEAAAAASUVORK5CYII=", 'base64')
+    const img = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAAUAAAAFAAQMAAAD3XjfpAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAGUExURRgYGOfn5wwjTt8AAABgSURBVGje7dexCQAgDEXBbOD+W7qB2ioWooKF97vA9eFFWVyAIAiCIAiCIPgpzNGW5hcIgiAInkJvGARBEARB8KjY+yUQBEEQvADHjp/0PQiCIAhuQ7kHgiAIgiAIvoIV8ju+nrUoQrAAAAAASUVORK5CYII=", 'base64')
     return {
       // We need to return 200 here or Firefox won’t display the image
       // HOWEVER a 200 means that if it times out on the first attempt it will stay the default image until the next build.
